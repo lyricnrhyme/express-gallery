b2de97b576beccd1b8b5a0d4a4d73a7bea8a8ec8
CwLauger <cwlauger@gmail.com>
2018-09-27 19:35:05 -1000

    seeded migrated

--- db/models/bookshelf.js
+++ db/models/bookshelf.js
@@ -1,2 +1,2 @@
-const knex = require('../knex');
+const knex = require('../../knex/knex');
 module.exports = require('bookshelf')(knex);
--- knex/seeds/02_gallery.js
+++ knex/seeds/02_gallery.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('gallery').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('gallery').insert([
+        {author:'Shakespear', link:'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiep4vg-tzdAhXJIDQIHbo0ABQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.mercurytheatre.co.uk%2Fevent%2Fwilliam-shakespeare-s-long-lost-first-play-abridged%2F&psig=AOvVaw3qj5Mw0SSzE194klcuOoG3&ust=1538198631019257', description:'shakespear is an effin Gee that writes smooth rhymes.'}
       ]);
     });
 };
