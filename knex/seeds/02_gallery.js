d2fd1610be486adc6be699dab475649d88eb8c9f
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-10-03 00:00:58 -1000

    YAS CRUD DUN

--- knex/seeds/02_gallery.js
+++ knex/seeds/02_gallery.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('gallery').insert([
-        {author:'Shakespear', link:'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiep4vg-tzdAhXJIDQIHbo0ABQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.mercurytheatre.co.uk%2Fevent%2Fwilliam-shakespeare-s-long-lost-first-play-abridged%2F&psig=AOvVaw3qj5Mw0SSzE194klcuOoG3&ust=1538198631019257', description:'shakespear is an effin Gee that writes smooth rhymes.'}
+        {author:'Shakespear', link:'https://d18lkz4dllo6v2.cloudfront.net/cumulus_uploads/entry/15220/shakespeare-for-kids%20Cropped.png?w=660', description:'shakespear is an effin Gee that writes smooth rhymes.'}
       ]);
     });
 };
--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -2,20 +2,13 @@ const express = require('express');
 const Router = express.Router();
 const Gallery = require('../db/models/gallery.js');
 
-//GET /
-//GET /gallery/:id
-//GET /gallery/new author, link, description
-//POST /gallery
-//GET /gallery/:id/edit author, link, description
-//PUT /gallery/:id
-//DELETE /gallery/:id
-
 Router.get('/', (req, res) => {
     Gallery
     .fetchAll()
     .then( gallery => {
-        res.json(gallery.serialize());
-        res.render('home', gallery);
+        let galleryThing = gallery.serialize()
+        console.log('pls', galleryThing);
+        res.render('home', {galleryThing});
     })
     .catch( err => {
         console.log('gallery err', err);
@@ -23,48 +16,97 @@ Router.get('/', (req, res) => {
     })
 })
 
+Router.get('/gallery/new', (req, res) => {
+    res.render('form');
+})
+
+Router.get('/gallery/:gallery_id', (req, res) => {
+    const { gallery_id } = req.params;
+    Gallery
+    .where({ gallery_id })
+    .fetchAll()
+    .then( gallery => {
+        let galleryDetail = gallery.toJSON()
+        res.render('detail', {galleryDetail});
+    })
+    .catch( err => {
+        console.log('detail err', err);
+        res.json(err);
+    })
+})
+
+Router.post('/gallery/new', (req, res) => {
+    const { gallery_id } = req.params;
+    const payload = {
+        author: req.body.author,
+        link: req.body.link,
+        description: req.body.description
+    }
+    Gallery
+    .forge(payload)
+    .save()
+    .then( result => {
+        res.redirect('/');
+    })
+    .catch( err => {
+        console.log('err', err)
+        res.json(err);
+    })
+})
+
+Router.put('/gallery/:gallery_id', (req, res) => {
+    const { gallery_id } = req.params;
+    const payload = {
+        author: req.body.author.trim(),
+        link: req.body.link.trim(),
+        description: req.body.description.trim()
+    }
+
+    Gallery
+    .where({gallery_id})
+    .fetch()
+    .then( gallery => {
+        return gallery.save(payload)
+    })
+    .then( result => {
+        res.redirect(`/gallery/${gallery_id}`)
+    })
+    .catch( err => {
+        res.json(err);
+    })
+})
+
+Router.delete('/gallery/:gallery_id', (req, res) => {
+    const {gallery_id} = req.params;
+    // console.log('gal', gallery_id);
+    Gallery
+    .where({gallery_id})
+    .destroy()
+    .then( result => {
+        res.redirect('/');
+    })
+    .catch( err => {
+        // console.log('wy doe', err);
+        res.json(err);
+    })
+})
+
+Router.get('/gallery/:gallery_id/edit', (req, res) => {
+    const { gallery_id } = req.params;
+    Gallery
+    .where({ gallery_id })
+    .fetchAll()
+    .then( gallery => {
+        let galleryUpdate = gallery.toJSON()
+        res.render('update', {galleryUpdate});
+    })
+    .catch( err => {
+        console.log('update err', err);
+        res.json(err);
+    })
+})
+
 module.exports = Router
-// // create task by user id
-// Router.post('/users/:user_id/tasks/new', (req, res) => {
-//     const { user_id } = req.params;
-//     const payload = {
-//       name: req.body.name
-//     }
-//     Tasks
-//       .forge(payload)
-//       .save()
-//       .then( result => {
-//         res.json(result)
-//       })
-//       .catch( err => {
-//         console.log('error', err)
-//         res.json(err);
-//       })
-//   })
-  
-//   // update task by task id
-//   Router.put('/tasks/:task_id/edit', (req, res) => {
-//     const { task_id } = req.params;
-    
-//     const payload = {
-//       name: req.body.name,
-//       is_complete: req.body.is_complete
-//     }
-  
-//     Tasks
-//       .where({task_id})
-//       .fetch()
-//       .then( task => {
-//         return task.save(payload)
-//       })
-//       .then( result => {
-//         console.log('result', result)
-//         res.json(result);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       })
-//   })
   
 //   // delete task by task id
 //   Router.delete('/tasks/:task_id/delete', (req, res) => {
