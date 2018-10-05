58fab5150b186c240f483909daf4f84cfe233649
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-10-04 19:51:19 -1000

    Switching with Carl

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,44 +1,51 @@
 const express = require('express');
 const Router = express.Router();
-const Users = require('../db/models/users.js')
+const Users = require('../db/models/users.js');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+//6:25
+
+passport.serializeUser( (user, done) => {
+    console.log('serial')
+    done(null, user.id)
+})
+
+passport.deserializeUser( (user, done) => {
+
+})
+
+passport.use(new LocalStrategy({usernameField: 'email'}, (username, password, done) => {
+    Users
+    .where({email})
+    .then( user => {
+        return bcrypt.compare(password, user.attributes.password)
+    })
+    .then( result => {
+        if (result) {
+            done(null, user.attributes)
+        } else {
+            done(null, false)
+        }
+    })
+    .catch( err => {
+        done(err)
+    })
+}))
 
 Router.post('/auth/register', (req, res) => {
     const {user_id, email, password} = req.body;
-        Users
-            .forge({user_id, email, password})
-            .save()
-            .then( result => {
-                if (result){
-                    //save user data into DB
-                    console.log('WELCOME')
-                }
-                else{
-                    res.send('YOU FUKKED UP')
-                }
-            })
-            .catch( err => {
-                console.log('error', err)
-                res.send(err)
-            })
+    bcrypt.hash(password, 10)
+    .then( hashedPassword => {
+        return Users
+        .forge({email, password: hashedPassword})
+        .save()
     })
-
-Router.get('/auth/login', (req, res) => {
-    res.render('login');
-})
-
-Router.post('/auth/login', (req, res) => {
-    const { email, password } = req.body;
-    Users
-    .where({ email })
-    .fetch()
-    .then( user => {
-        if (password === user.attributes.password) {
-            console.log('LOGGED IN')
-            req.session.isLoggedIn = true;
-            res.redirect('/')
+    .then( result => {
+        if (result) {
+            res.send('NEW USER')
         } else {
-            console.log('login err');
-            res.redirect('/auth/login')
+            res.send('ERRR')
         }
     })
     .catch( err => {
@@ -47,20 +54,25 @@ Router.post('/auth/login', (req, res) => {
     })
 })
 
+Router.get('/auth/login', (req, res) => {
+    res.render('login');
+})
+
+Router.post('/auth/login', passport.authenticate('local', {failreRedirect: '/'}), (req, res) => {
+    res.send('AUTHENTICATED')
+})
+
 Router.post('/auth/logout', (req, res) => {
-    console.log('LOGGED OUT')
-    req.session.destroy();
-    res.redirect('/login');
+    req.logout();
+    res.send('LOGGED OUT');
 })
 
 Router.get('/protected', (req, res) => {
     if (req.session.isLoggedIn) {
-        //can access
+        res.send('PASS')
     } else {
-        //no can access
+        res.send('NO PASS')
     }
-    res.send('PROTECTEDDDD');
-
 })
 
 module.exports = Router
